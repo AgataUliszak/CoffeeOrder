393e172c4733d465dd7c1cda92dd0cd5e8faa7f9
Agata Uliszak <agata.uliszak@ericsson.com>
2017-12-27 16:48:35 +0100

    Adding editText whith name and new checkbox

--- app/src/main/java/com/example/android/coffeeorder/MainActivity.java
+++ app/src/main/java/com/example/android/coffeeorder/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.android.coffeeorder;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import java.text.NumberFormat;
 import android.widget.CheckBox;
-import android.util.Log;
+import android.widget.Toast;
+import android.content.Intent;
+import java.lang.String;
+
 
 /**
  * This app displays an order form to order coffee.
@@ -24,39 +30,65 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the plus button is clicked
      */
     public void incrementOrder (View view) {
-        if (quantity>=0){
-            quantity +=1;
-            price= quantity*5;}
+        if (quantity==100){
+           //Show an error message as a toast
+            Toast.makeText(this, "You cannot have more than 100 coffes", Toast.LENGTH_SHORT).show();
+            return;
+            }
+        quantity +=1;
         displayQuantity (quantity);
-        displayPrice(price);
     }
 
     /**
      * This method is called when the minus button is clicked
      */
     public void decrementOrder (View view) {
-        if (quantity>0){
-            quantity -=1;
-            price= quantity*5;}
+        if (quantity==1){
+            //Show an error message as a toast
+            Toast.makeText(this, "You cannot have less than 1 coffe", Toast.LENGTH_SHORT).show();
+            return;
+           }
+        quantity -=1;
         displayQuantity (quantity);
-        displayPrice(price);
     }
 
     /**
      * This method is calculating a price
      */
-    private int calculatePrice () {return quantity * 5;}
-
+    private int calculatePrice (boolean addWhippedCream, boolean addChocolate) {
+     int basePrice=5;
+        if (addWhippedCream){
+            basePrice+=1;
+        }
+        if (addChocolate){
+            basePrice+=2;}
+        return basePrice*quantity;}
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        CheckBox whippedCreamCheckBox= (CheckBox) findViewById(R.id.whipped_cream_checbox);
+        CheckBox whippedCreamCheckBox= (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         boolean hasWhippedCream=whippedCreamCheckBox.isChecked();
 
-        int price=calculatePrice();
-        String priceMessage = createOrderSummary (price, hasWhippedCream);
-        displayMessage(priceMessage);
+        CheckBox chocolateCheckBox= (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate=chocolateCheckBox.isChecked();
+
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        String name= nameField.getText().toString();
+
+        int price=calculatePrice(hasWhippedCream, hasChocolate);
+        String priceMessage = createOrderSummary (name, price, hasWhippedCream, hasChocolate);
+
+//        Intent intent = new Intent(Intent.ACTION_SENDTO);
+//        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+//        intent.putExtra(Intent.EXTRA_SUBJECT, "Coffee order for "+name);
+//        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+//        if (intent.resolveActivity(getPackageManager()) != null) {
+//            startActivity(intent);
+//        }
+
+         displayMessage(priceMessage);
+
     }
 
     /**
@@ -84,10 +116,15 @@ public class MainActivity extends AppCompatActivity {
     }
     /**
      * This method display summary of the order
+     * @param price           of the order
+     * @param addWhippedCream is whether or not to add whipped cream to the coffee
+     * @param addChocolate    is whether or not to add chocolate to the coffee
+     * @return text summary
      */
-    private String createOrderSummary(int price, boolean addWheepedCream){
-        String priceMessage = "Name: Egi Uli";
-        priceMessage += "\nAdd whipped cream? "+ addWheepedCream;
+    private String createOrderSummary (String name, int price, boolean addWhippedCream, boolean addChocolate){
+        String priceMessage = "Name: "+ name;
+        priceMessage += "\nAdd whipped cream? "+ addWhippedCream;
+        priceMessage += "\nAdd chocolate? "+ addChocolate;
         priceMessage += "\nQuantity: " + quantity;
         priceMessage += "\nTotal: " + NumberFormat.getCurrencyInstance().format(price);
         priceMessage += "\nThank you!";
